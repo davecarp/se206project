ddd07e0f52780d8b4f450dfc05ef663c06d419cd
Tony Young <rofflwaffls@gmail.com>
2011-09-17 14:42:21 +1200

    Festival interoperability layer.

---
+++ spellorama/tts.py
@@ -0,0 +1,45 @@
+import multiprocessing
+import subprocess
+import os
+import signal
+
+class FestivalSlave(multiprocessing.Process):
+    """
+    Slave process for Festival.
+    """
+    def __init__(self, pipe, *args, **kwargs):
+        multiprocessing.Process.__init__(self, *args, **kwargs)
+        self.pipe = pipe
+        self.daemon = True
+
+    def run(self):
+        os.setsid()
+
+        while True:
+            text = self.pipe.recv()
+
+            proc = subprocess.Popen([ "festival", "--tts" ], stdin=subprocess.PIPE)
+            proc.stdin.write(text)
+            proc.stdin.close()
+
+class Festival(object):
+    def start(self):
+        """
+        Start the Festival connector.
+        """
+        self.pipe, endpoint = multiprocessing.Pipe()
+        self.proc = FestivalSlave(endpoint)
+        self.proc.start()
+
+    def speak(self, text):
+        """
+        Send the text to speak along the pipe to the slave.
+        """
+        self.pipe.send(text)
+
+    def panic(self):
+        """
+        Kill the slave and restart it.
+        """
+        os.killpg(os.getpgid(self.proc.pid), signal.SIGKILL)
+        self.start()
